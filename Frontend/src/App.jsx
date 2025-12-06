a5c171401f1f685b2247cf8e2e12bc5c314fb801
Madhav Khanal <madhavkhanal3145@gmail.com>
2025-12-06 01:02:56 -0500

    render redirect

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 import Header from './components/Header'
 import Home from './pages/Home'
@@ -29,7 +29,7 @@ function App() {
         <Header theme={theme} toggleTheme={toggleTheme} />
         <main className="main-content">
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<Navigate to="/blog" replace />} />
             <Route path="/blog" element={<BlogList />} />
             <Route path="/blog/:slug" element={<BlogPost />} />
             <Route path="/about" element={<About />} />
